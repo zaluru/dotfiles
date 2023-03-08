f6c51cfa2510dd6e978453a36eb7255b3fbbf05b
zaluru <zaluru@tutamail.com>
2023-03-08 23:43:17 +0100

    Added a note about discord microphone crackling sound

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -18,6 +18,9 @@
   system.stateVersion = "22.11";
 
   # Pipewire
+  # TODO there is some crackling noise in discord, propably related to those:
+  # https://www.reddit.com/r/linux_gaming/comments/vg8gl2/comment/id4liim/
+  # https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/2476
   # rtkit is optional but recommended
   security.rtkit.enable = true;
   services.pipewire = {
