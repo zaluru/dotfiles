421c71e042c5293b48a13815ab28a4e73aa472f4
zaluru <zaluru@tutamail.com>
2023-03-09 18:15:59 +0100

    Removed gtk theme for now

--- modules/gtk/default.nix
+++ modules/gtk/default.nix
@@ -11,10 +11,10 @@
       package = pkgs.papirus-icon-theme;
       name = "Papirus-Dark";
     };
-    theme = {
-      # TODO find how to properly set themes for gtk3 and gtk4
-      name = "adw-gtk3-dark";
-      package = pkgs.adw-gtk3; 
-    };
+#    theme = {
+#      # TODO find how to properly set themes for gtk3 and gtk4
+#      name = "adw-gtk3-dark";
+#      package = pkgs.adw-gtk3; 
+#    };
   };
 }
