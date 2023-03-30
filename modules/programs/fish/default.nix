32a4080e9429206775cb2447d5fad3796c3effb5
zaluru <zaluru@tutamail.com>
2023-03-30 16:17:59 +0200

    Added missing semicolons

--- modules/programs/fish/default.nix
+++ modules/programs/fish/default.nix
@@ -10,8 +10,8 @@
       cleanup = "nix-collect-garbage && nix-collect-garbage -d && doas nix-collect-garbage && doas nix-collect-garbage -d && doas rm /nix/var/nix/gcroots/auto/*";
       n = "clear && neofetch";
       v = "nvim";
-      vi = "nvim"
-      vim = "nvim"
+      vi = "nvim";
+      vim = "nvim";
       record = "wf-recorder -f $HOME/Videos/$(date +'%Y%m%d%H%M%S_1.mp4')";
     };
     
