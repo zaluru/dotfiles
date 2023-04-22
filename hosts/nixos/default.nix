c2f273597cae71a232b1e1d3ef53e1943a0cf8c3
zaluru <zaluru@tutamail.com>
2023-04-22 17:38:28 +0200

    Added xdg-desktop-portal-hyprland
    I will probably have to disable the default xdg-desktop-portal,
    but running both at the same time works for now.

--- hosts/nixos/default.nix
+++ hosts/nixos/default.nix
@@ -48,7 +48,7 @@
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    extraPortals = [ inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland ];
     #gtkUsePortal = true;
   };
 
