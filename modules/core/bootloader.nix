a2210374534ca994f783378c5db9f82e5f5c22b7
zaluru <zaluru@tutamail.com>
2023-06-04 21:04:56 +0200

    Fixed wrong syntax for boot.loader

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,26 +2,28 @@
 
 {
   # bootloader configuration
-  boot.cleanTmpDir = true;
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" "vfio-pci" ];
-  boot.kernelParams = [ "intel_iommu=on" "iommu=pt" ];
-  boot.extraModulePackages = [ ];
-  boot.supportedFilesystems = [ "ntfs" ];
-  #boot.kernelPackages = ;
-  loader = {
-    systemd-boot.enable = false;
-    efi.canTouchEfiVariables = true;
-    timeout = 1;
-    grub = {
-      enable = true;
-      useOSProber = true;
-      efiSupport = true;
-      device = "nodev";
-      theme = null;
-      backgroundColor = null;
-      splashImage = null;
+  boot = {
+    cleanTmpDir = true;
+    initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" "rtsx_usb_sdmmc" ];
+    initrd.kernelModules = [ ];
+    kernelModules = [ "kvm-intel" "vfio-pci" ];
+    kernelParams = [ "intel_iommu=on" "iommu=pt" ];
+    extraModulePackages = [ ];
+    supportedFilesystems = [ "ntfs" ];
+    #kernelPackages = ;
+    loader = {
+      systemd-boot.enable = false;
+      efi.canTouchEfiVariables = true;
+      timeout = 1;
+      grub = {
+        enable = true;
+        useOSProber = true;
+        efiSupport = true;
+        device = "nodev";
+        theme = null;
+        backgroundColor = null;
+        splashImage = null;
+      };
     };
   };
 }
