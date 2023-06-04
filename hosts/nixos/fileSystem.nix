6bb9663e0d26260dd5a6c9133fa6c8af62daeceb
zaluru <120514027+zaluru@users.noreply.github.com>
2023-06-04 13:30:02 +0000

    Change file systems to labels rather than uuid

--- hosts/nixos/fileSystem.nix
+++ hosts/nixos/fileSystem.nix
@@ -10,94 +10,94 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  boot.initrd.luks.devices."ssd".device = "/dev/disk/by-label/nebula-cryptroot";
+  
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=nixos-root" ];
     };
 
-  boot.initrd.luks.devices."ssd".device = "/dev/disk/by-uuid/125357d9-611c-4c9e-bac4-45ba531158a2";
-
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=nixos-home" ];
     };
 
   fileSystems."/var" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=nixos-var" ];
     };
 
   fileSystems."/home/zaluru/books" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=books" ];
     };
 
   fileSystems."/home/zaluru/dev" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=development" ];
     };
 
   fileSystems."/home/zaluru/documents" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=documents" ];
     };
 
   fileSystems."/home/zaluru/downloads" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=downloads" ];
     };
 
   fileSystems."/home/zaluru/games" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=games" ];
     };
 
   fileSystems."/home/zaluru/.local/share/keepassxc" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=keepassxc" ];
     };
 
   fileSystems."/home/zaluru/music" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=music" ];
     };
 
   fileSystems."/home/zaluru/pictures" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=pictures" ];
     };
 
   fileSystems."/home/zaluru/.ssh" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=ssh" ];
     };
 
   fileSystems."/home/zaluru/torrents" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=torrents" ];
     };
 
   fileSystems."/home/zaluru/videos" =
-    { device = "/dev/disk/by-uuid/0a1d8011-b515-4cec-89e2-05eb50ea6073";
+    { device = "/dev/disk/by-label/nebula-root";
       fsType = "btrfs";
       options = [ "subvol=videos" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/7F9D-D35B";
+    { device = "/dev/disk/by-label/nebula-boot";
       fsType = "vfat";
     };
 
