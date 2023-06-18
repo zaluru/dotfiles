2c326b3825f22621bcc0e82c92434a20490747a5
zaluru <zaluru@tutamail.com>
2023-06-18 19:12:26 +0200

    Added substitutes

--- modules/core/nix.nix
+++ modules/core/nix.nix
@@ -11,6 +11,13 @@
       dates = "daily";
       options = "--delete-older-than 7d";
     };
+
+    # TODO have to learn how this works exactly
+    # pin the registry to avoid downloading and evaling a new nixpkgs version every time
+    registry = lib.mapAttrs (_: v: {flake = v;}) inputs;
+
+
+
     settings = {
       auto-optimise-store = true;
       # allow sudo users to mark the following values as trusted
@@ -19,6 +26,29 @@
       trusted-users = ["@wheel"];
       
       experimental-features = ["nix-command" "flakes"];
+
+      # use binary cache
+      builders-use-substitutes = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://fortuneteller2k.cachix.org"
+        "https://nixpkgs-wayland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://webcord.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "fortuneteller2k.cachix.org-1:kXXNkMV5yheEQwT0I4XYh1MaCSz+qg72k8XAi2PthJI="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "webcord.cachix.org-1:l555jqOZGHd2C9+vS8ccdh8FhqnGe8L78QrHNn+EFEs="
+        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+      ];
+
     };
   };
 }
