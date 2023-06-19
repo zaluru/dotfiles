a98f89745ca8522e873c37a73e31086ffbeddb34
zaluru <zaluru@tutamail.com>
2023-06-19 20:42:00 +0200

    Added locale and timezone settings

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -10,4 +10,27 @@
     memoryPercent = 50;
     algorithm = "zstd";
   };
+
+  i18n = let
+    defaultLocale = "en_US.UTF-8";
+    pl = "pl_PL.UTF-8";
+  in {
+    inherit defaultLocale;
+    extraLocaleSettings = {
+      LANG = defaultLocale;
+      LC_COLLATE = defaultLocale;
+      LC_CTYPE = defaultLocale;
+      LC_MESSAGES = defaultLocale;
+
+      LC_ADDRESS = pl;
+      LC_IDENTIFICATION = pl;
+      LC_MEASUREMENT = pl;
+      LC_MONETARY = pl;
+      LC_NAME = pl;
+      LC_NUMERIC = pl;
+      LC_PAPER = pl;
+      LC_TELEPHONE = pl;
+      LC_TIME = pl;
+    };
+  };
 }
