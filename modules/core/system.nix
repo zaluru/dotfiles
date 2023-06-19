83f34fc3c4ad7a42d1c2da6704868968d62b009b
zaluru <zaluru@tutamail.com>
2023-06-19 21:02:59 +0200

    Added oomd.

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -41,5 +41,13 @@
   in {
     inherit extraConfig;
     user = {inherit extraConfig;};
+
+    # Systemd OOMd
+    # Fedora enables these options by default. See the 10-oomd-* files here:
+    # https://src.fedoraproject.org/rpms/systemd/tree/acb90c49c42276b06375a66c73673ac3510255
+    oomd = {
+      enableRootSlice = true;
+      enableUserServices = true;
+    };
   };
 }
