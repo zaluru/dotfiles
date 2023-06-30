c4456f7bb7933333605818150fbd8dca6a250d4e
zaluru <zaluru@tutamail.com>
2023-06-30 23:55:28 +0200

    Moving hardware settings that can be shared between hosts

--- hosts/aurora/hardware.nix
+++ hosts/aurora/hardware.nix
@@ -2,13 +2,6 @@
 
 {
   hardware.pulseaudio.enable = false;
-  #hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  hardware.opengl.driSupport32Bit = true;
-  #hardware.opengl.extraPackages = with pkgs; [
-  #  amdvlk
-  #];
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.nvidia.modesetting.enable = true;
 }
