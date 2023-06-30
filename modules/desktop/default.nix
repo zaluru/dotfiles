aa9032f984f372d0cbc824570b927c09afe4223a
zaluru <zaluru@tutamail.com>
2023-07-01 00:26:11 +0200

    Some fixes to variables and portals

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -2,6 +2,7 @@
   config,
   pkgs,
   inputs,
+  lib,
   ...
 }: {
   #imports = [./fonts.nix ./services.nix ./pipewire.nix];
@@ -19,7 +20,6 @@
       #VISUAL="emacsclient -c -a emacs";
       BROWSER = "firefox";
       TERMINAL = "kitty";
-      GTK_USE_PORTAL = "1";
       NIXOS_OZONE_WL = "1";
       QT_SCALE_FACTOR = "1";
       MOZ_ENABLE_WAYLAND = "1";
@@ -27,7 +27,7 @@
       XDG_SESSION_TYPE = "wayland";
       XDG_CURRENT_DESKTOP = "Hyprland";
       XDG_SESSION_DESKTOP = "Hyprland";
-      _JAVA_AWT_WM_NONREPARENTING=1;
+      _JAVA_AWT_WM_NONREPARENTING= "1";
       __GL_GSYNC_ALLOWED = "0";
       __GL_VRR_ALLOWED = "0";
       _JAVA_AWT_WM_NONEREPARENTING = "1";
@@ -40,8 +40,8 @@
       QT_AUTO_SCREEN_SCALE_FACTOR = "1";
       QT_QPA_PLATFORM = "wayland";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-      QT_QPA_PLATFORMTHEME = "qt5ct";
-      QT_STYLE_OVERRIDE = "kvantum";
+      QT_QPA_PLATFORMTHEME = lib.mkForce "qt5ct";
+      QT_STYLE_OVERRIDE = lib.mkForce "kvantum";
       WLR_BACKEND = "vulkan";
       WLR_RENDERER = "vulkan";
       WLR_NO_HARDWARE_CURSORS = "1";
@@ -74,7 +74,8 @@
     enable = true;
     wlr.enable = false;
     extraPortals = [
-      pkgs.xdg-desktop-portal-gtk
+      # TODO conflicts with the hyprland portal
+      #pkgs.xdg-desktop-portal-gtk
       inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
     ];
   };
