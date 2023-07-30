eff2caf1a2209f1caf93c2fc6d3d8f015bc7339b
zaluru <zaluru@tutamail.com>
2023-07-30 14:00:43 +0200

    Enabled starship integrations for bash, zsh and fish^

--- modules/home/programs/starship/default.nix
+++ modules/home/programs/starship/default.nix
@@ -3,6 +3,9 @@
 {
   programs.starship = {
     enable = true;
+    enableFishIntegration = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
   };
   xdg.configFile."starship.toml".source = ./starship.toml;
 }
