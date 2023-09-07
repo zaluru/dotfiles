670875bf09fd72cfb0ac6fc3120afc7ec13a9584
zaluru <zaluru@tutamail.com>
2023-09-07 13:50:12 +0200

    Added nfs support to vagrant, WIP

--- modules/virtualisation/vagrant.nix
+++ modules/virtualisation/vagrant.nix
@@ -5,19 +5,45 @@
 {
   environment.systemPackages = with pkgs; [
     vagrant
+    nfs-utils
   ];
   # https://nixos.wiki/wiki/Vagrant#Using_NFS_mounts
   # Minimal configuration for NFS support with Vagrant.
   services.nfs.server.enable = true;
-  
-  # Add firewall exception for VirtualBox provider 
-  networking.firewall.extraCommands = ''
-    ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
-  '';
 
-  # Add firewall exception for libvirt provider when using NFSv4 
-  networking.firewall.interfaces."virbr1" = {                                   
-    allowedTCPPorts = [ 2049 ];                                               
-    allowedUDPPorts = [ 2049 ];                                               
-  };     
+  # the above does not seem to be enought
+  boot.initrd = {
+    supportedFilesystems = [ "nfs" ];
+    kernelModules = [ "nfs" ];
+  };
+
+  
+  # TODO have to figure out what firewall rules are required for nfs and vagrant, for now have to turn off the firewall
+  # Add firewall exception for VirtualBox provider 
+  networking = {
+    firewall = {
+      extraCommands = ''
+        ip46tables -I INPUT 1 -i vboxnet+ -p tcp -m tcp --dport 2049 -j ACCEPT
+        
+        # allow NFSv3, NFSv4, mountd, rpcbind for nfs mounting
+        ip46tables -A INPUT -i vboxnet+ -p tcp -m multiport --sports 2049,111,2048 -j ACCEPT
+        ip46tables -A INPUT -i vboxnet+ -p udp -m multiport --sports 2049,111,2048 -j ACCEPT
+        ip46tables -A INPUT -i virbr+ -p tcp -m multiport --sports 2049,111,2048 -j ACCEPT
+        ip46tables -A INPUT -i virbr+ -p udp -m multiport --sports 2049,111,2048 -j ACCEPT
+
+        # allow NFSv3, NFSv4, mountd, rpcbind for nfs mounting
+        ip46tables -A INPUT -i vboxnet+ -p tcp -m multiport --dports 2049,111,2048 -j ACCEPT
+        ip46tables -A INPUT -i vboxnet+ -p udp -m multiport --dports 2049,111,2048 -j ACCEPT
+        ip46tables -A INPUT -i virbr+ -p tcp -m multiport --dports 2049,111,2048 -j ACCEPT
+        ip46tables -A INPUT -i virbr+ -p udp -m multiport --dports 2049,111,2048 -j ACCEPT
+
+      '';
+
+      # Add firewall exception for libvirt provider when using NFSv4 
+      interfaces."virbr1" = {                                   
+        allowedTCPPorts = [ 2049 ];                                               
+        allowedUDPPorts = [ 2049 ];                                               
+      };     
+    };
+  };
 }
