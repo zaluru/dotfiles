cd034f108115dc98fcf68169bf874d5acf428929
zaluru <zaluru@tutamail.com>
2023-12-22 23:20:44 +0100

    Removed configuration made redundant by keychain

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -29,7 +29,6 @@
       __GL_GSYNC_ALLOWED = "0";
       __GL_VRR_ALLOWED = "0";
       _JAVA_AWT_WM_NONEREPARENTING = "1";
-      SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
       DISABLE_QT5_COMPAT = "0";
       GDK_BACKEND = "wayland";
       ANKI_WAYLAND = "1";
@@ -50,8 +49,6 @@
     };
     loginShellInit = ''
       dbus-update-activation-environment --systemd DISPLAY
-      eval $(gnome-keyring-daemon --start --components=ssh)
-      eval $(ssh-agent)
     '';
   };
 
