9f7fc9fe013e51fd6a695b6767fb3fce49c60e80
zaluru <zaluru@tutamail.com>
2023-12-22 23:15:02 +0100

    Added keychain for ssh-agent

--- modules/home/programs/ssh/default.nix
+++ modules/home/programs/ssh/default.nix
@@ -6,4 +6,11 @@
     enable = true;
     package = pkgs.openssh;
   };
+  programs.keychain = {
+    enable = true;
+    enableBashIntegration = true;
+    enableZshIntegration = true;
+    enableFishIntegration = true;
+    enableNushellIntegration = true;
+  };
 }
