daecd233c1d66a950dc06a76caaaf918c2ce36bb
zaluru <zaluru@tutamail.com>
2024-01-06 16:35:23 +0100

    Disabled 32bit support temporarly for testing arm builds

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -53,7 +53,7 @@
     opengl = {
       enable = true;
       driSupport = true;
-      driSupport32Bit = true;
+      #driSupport32Bit = true;
       extraPackages = with pkgs; [
         vaapiVdpau
         libvdpau-va-gl
