7add3e6eebd0e5d6fc3a334f23a6775523339d5f
zaluru <zaluru@tutamail.com>
2024-02-17 01:21:40 +0100

    wall-change - added image picker when no path is provided

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -18,7 +18,7 @@ let
           --separator-color 00000000 \
           --fade-in 0.3
   '';
-  wall-change = pkgs.writeShellScriptBin "wall-change" (builtins.readFile ./udev_on_bat.sh);
+  wall-change = pkgs.writeShellScriptBin "wall-change" (builtins.readFile ./wall-change.sh.sh);
 in
 {
 
