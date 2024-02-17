f526a2f2fd10ee2eecd0341c3f4f0b8f4f8e4adc
zaluru <zaluru@tutamail.com>
2024-02-17 01:37:25 +0100

    wall-change - changed path to XDG_PICTURES_DIR/wallpapers

--- modules/home/scripts/wall-change.sh
+++ modules/home/scripts/wall-change.sh
@@ -1,5 +1,5 @@
 if [[ -z "$1" ]]; then
-  IMAGE="$(imv "${XDG_DATA_HOME:-$HOME/.local/share}"/wallpapers -r -w imv-wallpaper-change -c 'bind <Return> exec echo $imv_current_file; quit')"
+  IMAGE="$(imv "${XDG_PICTURES_DIR:-$HOME/pictures}"/wallpapers -r -w imv-wallpaper-change -c 'bind <Return> exec echo $imv_current_file; quit')"
 else
   IMAGE=$1
 fi 
