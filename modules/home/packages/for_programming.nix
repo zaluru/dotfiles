6a8b48494a2f4e066202bcee9bce28a3336a6f90
zaluru <zaluru@tutamail.com>
2024-02-25 02:10:03 +0100

    chore: removed unnecessary packages

--- modules/home/packages/for_programming.nix
+++ modules/home/packages/for_programming.nix
@@ -3,17 +3,12 @@
 
 {
   home.packages = with pkgs; [
-  nodejs
-  nodePackages.nodemon
-  yarn
-  gcc
-  nix-prefetch-github
-  vscodium-fhs
-  devbox
-  just
-  asdf-vm
-  typescript
-  rnix-lsp
-  # rust
+    gcc
+    nix-prefetch-github
+    vscodium-fhs
+    devbox
+    just
+    asdf-vm
+    rnix-lsp
   ];
 }
