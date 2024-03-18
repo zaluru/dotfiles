b04f0653283781392af7e7d99b75c6e83eca3580
zaluru <zaluru@zaluru.com>
2024-03-19 00:07:25 +0100

    fix: changed the port for obs-web

--- modules/server/obs-web.nix
+++ modules/server/obs-web.nix
@@ -3,7 +3,7 @@
 {
   virtualisation.oci-containers.containers."obs-web" = {
     autoStart = true;
-    ports = [ "5000:5000" ];
+    ports = [ "5050:5000" ];
     image = "ghcr.io/niek/obs-web:latest";
     volumes = [ ];
     extraOptions = [ ];
