63d18a83663d0a2ebf55badf0112d15efffa9b52
zaluru <zaluru@zaluru.com>
2024-04-12 00:33:28 +0200

    feat: initial commit for a working nixos-generators config
    needs to be cleaned up

--- flake.nix
+++ flake.nix
@@ -54,6 +54,15 @@
     let
       username = "zaluru";
       selfPkgs = import ./pkgs;
+
+      pkgsForSystem = system: import nixpkgs {
+        inherit system;
+      };
+      allVMs = [ "x86_64-linux" "aarch64-linux" ];
+      forAllVMs = f: nixpkgs.lib.genAttrs allVMs (system: f {
+        inherit system;
+        pkgs = pkgsForSystem system;
+      });
     in
     {
       overlays.default = selfPkgs.overlay;
@@ -72,17 +81,49 @@
             ;
         }
       );
-      packages.x86_64-linux = {
-        proxmox = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          modules = [ ./configuration.nix ];
-          format = "proxmox";
+      packages = forAllVMs ({ system, pkgs }: {
+        vm = nixos-generators.nixosGenerate {
+          system = system;
+          specialArgs = {
+            diskSize = 50 * 1024;
+            inherit
+              system
+              self
+              inputs
+              nixpkgs
+              username
+              astronvim
+              hyprland
+              disko
+              zjstatus
+              nixos-wsl;
+          };
+          modules = [ (import ./hosts/phobos) ]
+          ++ [
+            { networking.hostName = "phobos"; }
+            inputs.home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useUserPackages = true;
+                useGlobalPkgs = true;
+                extraSpecialArgs = {
+                  inherit inputs username astronvim;
+                };
+                users.zaluru = {
+                  imports = [ (import ./hosts/home-zaluru.nix) ];
+                };
+              };
+              nixpkgs = {
+                overlays = [ self.overlays.default ];
+              };
+            }
+            ./modules/core
+            ./modules/virtualisation
+            # Secrets management
+            inputs.agenix.nixosModules.default
+          ];
+          format = "qcow";
         };
-        aws = nixos-generators.nixosGenerate {
-          system = "x86_64-linux";
-          modules = [ ./configuration.nix ];
-          format = "amazon";
-        };
-      };
+      });
     };
 }
