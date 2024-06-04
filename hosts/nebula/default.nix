27c2ab345dc791692bbf169b15c7ca957d244052
zaluru <zaluru@zaluru.com>
2024-06-04 21:02:04 +0200

    feat: added some nvidia specific settings
    Signed-off-by: zaluru <zaluru@zaluru.com>

--- hosts/nebula/default.nix
+++ hosts/nebula/default.nix
@@ -1,4 +1,4 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, config, ... }:
 
 {
   imports =
@@ -60,5 +60,62 @@
     enable = true;
     memoryPercent = 50;
     algorithm = "zstd";
+  };
+
+#  environment.sessionVariables = {
+#      LIBVA_DRIVER_NAME = "nvidia";
+#      XDG_SESSION_TYPE = "wayland";
+#      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+#      GBM_BACKEND = "nvidia-drm";
+#      WLR_NO_HARDWARE_CURSORS = "1";
+#      # WLR_RENDERER = "vulkan";
+#      # GTK_USE_PORTAL = "1";
+#      # NIXOS_XDG_OPEN_USE_PORTAL = "1";
+#      NIXOS_OZONE_WL = "1";
+#      XWAYLAND_NO_GLAMOR = "1";
+#  };
+
+  hardware = {
+      opengl = {
+        enable = true;
+        driSupport = true;
+        driSupport32Bit = true;
+      };
+
+      # Most wayland compositors need this
+      # nvidia.modesetting.enable = true;
+      # nvidia.open = false;
+  };
+
+  hardware.nvidia = {
+
+    # Modesetting is required.
+    modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
+    # Fine-grained power management. Turns off GPU when not in use.
+    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+    powerManagement.finegrained = false;
+
+    # Use the NVidia open source kernel module (not to be confused with the
+    # independent third-party "nouveau" open source driver).
+    # Support is limited to the Turing and later architectures. Full list of 
+    # supported GPUs is at: 
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Only available from driver 515.43.04+
+    # Currently alpha-quality/buggy, so false is currently the recommended setting.
+    open = false;
+
+    # Enable the Nvidia settings menu,
+    # accessible via `nvidia-settings`.
+    nvidiaSettings = true;
+
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
