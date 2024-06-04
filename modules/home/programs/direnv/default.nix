d76e22789a475ed2d6df39623f3f0ad35a5a19ac
zaluru <zaluru@zaluru.com>
2024-06-04 16:03:23 +0200

    fix: updated direnv config
    Signed-off-by: zaluru <zaluru@zaluru.com>

--- modules/home/programs/direnv/default.nix
+++ modules/home/programs/direnv/default.nix
@@ -1,7 +1,9 @@
 { ... }:
 
 {
-  programs.direnv.enable = true;
-  programs.direnv.nix-direnv.enable = true;
-  xdg.configFile."direnv".source = ./config;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
+  xdg.configFile."direnv/direnvrc".source = ./config/direnvrc;
 }
