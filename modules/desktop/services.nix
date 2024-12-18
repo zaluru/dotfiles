f17c7ca16510a569fa20ac49f64f96bf16d766fa
zaluru <zaluru@zaluru.com>
2024-12-18 01:00:10 +0100

    Updated seatd service

--- modules/desktop/services.nix
+++ modules/desktop/services.nix
@@ -1,15 +1,20 @@
 { pkgs, ... }:
 
 {
-  systemd.services = {
-    seatd = {
-      enable = true;
-      description = "Seat management daemon";
-      script = "${pkgs.seatd}/bin/seatd -g wheel";
-      serviceConfig = {
-        Type = "simple";
-        Restart = "always";
-        RestartSec = "1";
+  systemd = {
+    services = {
+      seatd = {
+        enable = true;
+        description = "Seat management daemon";
+        script = "${pkgs.seatd}/bin/seatd -g wheel";
+        serviceConfig = {
+          Type = "simple";
+          Restart = "always";
+          RestartSec = "1";
+        };
+        wantedBy = [ "multi-user.target" ];
+      };
+    };
       };
       wantedBy = [ "multi-user.target" ];
     };
