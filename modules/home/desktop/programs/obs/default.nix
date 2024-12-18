2a14b209958b21429d7f85ddea1fddf29ffc669b
zaluru <zaluru@zaluru.com>
2024-12-18 01:04:58 +0100

    Removed nix config for obs, using flatpak version

--- modules/home/desktop/programs/obs/default.nix
+++ modules/home/desktop/programs/obs/default.nix
@@ -1,7 +1,16 @@
 { ... }:
 
 {
-  programs.obs-studio = {
-    enable = true;
-  };
+  #programs.obs-studio = {
+  #  enable = true;
+  #};
+
+  #services.flatpak = {
+  #  packages = [
+  #    "com.obsproject.Studio"
+  #    # Capture vulkan and opengl games bypassing the compositor - https://github.com/nowrep/obs-vkcapture
+  #    "com.obsproject.Studio.Plugin.OBSVkCapture"
+  #    "org.freedesktop.Platform.VulkanLayer.OBSVkCapture"
+  #  ];
+  #};
 }
